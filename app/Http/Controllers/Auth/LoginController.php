ae82c0418b7e75086cd264306de679c2a3496d57
Edgar Yesid Rangel Pieschacon <jefelitman@gmail.com>
2018-07-24 10:34:54 -0500

    DB v1.0

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Auth;
+
+class LoginController extends Controller
+{
+    public function autentication(){
+        $credentials = $this->validate(request(), [
+            'email' => 'email|required|string',
+            'password' => 'required|string'
+        ]);
+
+        if(Auth::attempt($credentials)){
+            return redirect()->route('init');
+        }else{
+            return back()
+                ->withErrors(['email' => trans('auth.failed')])
+                ->withInput(request(['email']));
+        }
+    }
+
+    public function login(){
+        return view('auth.login');
+    }
+
+    public function init(){
+        return view('init')
+    }
+}
