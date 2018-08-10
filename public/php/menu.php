48860bf6b53473bdb27e81842d62a504fd0ea1d0
Edgar Yesid Rangel Pieschacon <jefelitman@gmail.com>
2018-08-10 00:12:29 -0500

    Primera version visual bonita
    Esta es la primera version visual bonita para el proyecto de colcief

---
+++ public/php/menu.php
@@ -0,0 +1,23 @@
+<!-- Menu principal para la pagina (Ira en php) -->
+<nav class="green darken-4">
+        <div class="nav-wrapper">
+            <!-- Espacio donde estara el logo de la pagina -->
+            <a href="#!" class="brand-logo">ColCief</a>
+            <!-- Link que hace referencia al menu responsivo -->
+            <a href="#" data-target="menu-responsivo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+            <!-- Menu normal en modo grande -->
+            <ul class="right hide-on-med-and-down">
+                <li><a href="#!"><i class="material-icons right">home</i>Inicio</a></li>
+                <li><a href="#!"><i class="material-icons right">school</i>Nuestro Colegio</a></li>
+                <li><a href="#!"><i class="material-icons right">local_phone</i>Contáctenos</a></li>
+                <li><a href="http://colcief.com/login"><i class="material-icons">account_circle</i></a></li>
+            </ul>
+            <!-- Menu responsive en modo movil -->
+            <ul class="sidenav" id="menu-responsivo">
+                <li><a href="#!"><i class="material-icons right">home</i>Inicio</a></li>
+                <li><a href="#!"><i class="material-icons right">school</i>Nuestro Colegio</a></li>
+                <li><a href="#!"><i class="material-icons right">local_phone</i>Contáctenos</a></li>
+                <li><a href="http://colcief.com/login"><i class="material-icons right">account_circle</i>Iniciar Sesión</a></li>
+            </ul>
+        </div>
+    </nav>
