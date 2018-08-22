4ca4a730ab7a07aac5d7db3d11f0c26326e2219c
Edgar Yesid Rangel Pieschacon <jefelitman@gmail.com>
2018-08-22 11:03:39 -0500

    Creating a Layout for the project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="{{ asset('css/materialize.min.css') }}"  media="screen,projection"/>
+@extends('layout')
 
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-</head>
-<body>
-    
-    <?PHP
-		include("menu.php")
-	?>
-    
-    <!--Galeria por medio del materialize-->
-	<div class="container">
-		<div class="row">
+@section('content')
+<div class="row">
 			<div class="col s12">
 				<div class="slider">
 					<ul class="slides">
@@ -27,23 +11,7 @@
 						<li><img src="#"></li>
 						<li><img src="#"></li>
 					</ul>
-				</div>
 			</div>
-		</div>
 	</div>
-    
-    <?PHP
-		include("footer.php")
-	?>
-    
-    <!--JavaScript at end of body for optimized loading-->
-    <script type="text/javascript" src="{{ asset('js/jquery.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('js/materialize.min.js') }}"></script>
-    <script type="text/javascript">
-        $(document).ready(function(){
-            $('.sidenav').sidenav();
-            $('.slider').slider();
-        });
-    </script>
-</body>
-</html>
+</div>
+@endsection
