522953d49e39a639cc5092a6d8995b5ed3788774
piiok <33143055+piiok@users.noreply.github.com>
2018-09-18 13:21:30 -0500

    Adelanto Materia

---
+++ resources/views/materias/verMateria.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Estudiante</title>
+</head>
+<body>
+	{{-- Guia Front --}}
+	{{-- Se envía el objeto $estudiante y el objeto $acudiente --}}
+	{{-- Variables enviadas desde Local>App>Http>Controllers>EstudianteController.php  funcion show() 
+		 @Autor Paola C. --}}
+	{{-- URL: localhost:8000\estudiantes\{pk_estudiante} --}}
+	<h3>Tipo de Archivos:</h3> $materia: {{ gettype($materia)}} <br>
+	<h3>Contenido materia:</h3> {{$materia}} <br>
+
+	{{-- <h1>Ejemplos</h1>
+	Nombre: {{$estudiante->nombre}} <br>
+	Apellido: {{$estudiante->apellido}} <br> --}}
+
+</body>
+</html>
