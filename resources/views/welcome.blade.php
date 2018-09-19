aaca9a879fa484d2244b06380f0d40b19fc71a5a
Karen <karen_vivianasis@hotmail.com>
2018-09-19 16:11:13 -0500

    Front

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,23 @@
-@extends('content')
-
-@section('content')
-<div class="row">
+@include('header')
+@include('menu')
+<br>
+<br>
+<!--Galeria por medio del materialize-->
+	<div class="container">
+		<div class="row">
 			<div class="col s12">
 				<div class="slider">
 					<ul class="slides">
-						<li><img src="#"></li>
-						<li><img src="#"></li>
-						<li><img src="#"></li>
-						<li><img src="#"></li>
-						<li><img src="#"></li>
+                        <li><img class="responsive-img" src="css/img/1.jpeg"></li>
+                        <li><img  class="responsive-img"src="css/img/2.jpeg"></li>
+                        <li><img class="responsive-img" src="css/img/3.jpeg"></li>
+                        <li><img class="responsive-img" src="css/img/4.jpeg"></li>
+                        <li><img class="responsive-img" src="css/img/5.jpeg"></li>
+                        <li><img class="responsive-img" src="css/img/6.jpeg"></li>
+                        <li><img class="responsive-img" src="css/img/7.jpeg"></li>
 					</ul>
+				</div>
 			</div>
-	</div>
-</div>
-@endsection
+		</div>
+     </div>
+@include('pie')
