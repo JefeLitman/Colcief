45752822ea467ca9f30051e3ca83f5c75bfe3e7c
Karen <karen_vivianasis@hotmail.com>
2018-10-18 16:00:38 -0500

    cambios del menu principal

--- resources/views/menus/principal.blade.php
+++ resources/views/menus/principal.blade.php
@@ -6,20 +6,22 @@
         {{--  boton de menu para el responsive  --}}
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             {{--  menu  --}}
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                 {{--  <!--Link que hace referencia al menu responsivo -->
                 <a href="#" data-target="menu-responsivo" class="sidenav-trigger"><i class="material-icons">menu</i></a>  --}}
-                <!--Menu normal en modo grande -->
-                <ul class="nav navbar-nav navbar-right mr-auto"> {{-- id="menu" --}}
-                    {{--  Inicio  --}}
-                    <li class="nav-item"><a class="nav-link" href="{{ url('/') }}" style="color: #ffffff;">&nbsp &nbsp Inicio &nbsp &nbsp<span class="sr-only">(current)</span><i class="material-icons right">home</i></a></li>
-                    {{--  Nuestro Colegio  --}}
-                    <li class="nav-item"><a class="nav-link" href="{{ url('/') }}" style="color: #ffffff;">&nbsp &nbsp Nuestro Colegio &nbsp &nbsp<i class="material-icons right">school</i></a></li>
-                    {{--  Contactenos  --}}
-                    <li class="nav-item"><a class="nav-link" href="{{ url('/') }}" style="color: #ffffff;">&nbsp &nbsp Contactenos &nbsp &nbsp<i class="material-icons right">local_phone</i></a></li>
-                    {{--  Login  --}}
-                    <li class="nav-item"><a class="nav-link" href="{{ url('/login') }}" style="color: #ffffff;">&nbsp &nbsp Login &nbsp &nbsp<i class="material-icons right">account_circle</i></a></li>
+                <div>
+                    <!--Menu normal en modo grande -->
+                    <ul class="nav navbar-nav navbar-right mr-auto"> {{-- id="menu" --}}
+                        {{--  Inicio  --}}
+                        <li class="nav-item"><a class="nav-link" href="{{ url('/') }}" style="color: #ffffff;">&nbsp &nbsp Inicio &nbsp &nbsp<span class="sr-only">(current)</span><i class="material-icons right">home</i></a></li>
+                        {{--  Nuestro Colegio  --}}
+                        <li class="nav-item"><a class="nav-link" href="{{ url('/') }}" style="color: #ffffff;">&nbsp &nbsp Nuestro Colegio &nbsp &nbsp<i class="material-icons right">school</i></a></li>
+                        {{--  Contactenos  --}}
+                        <li class="nav-item"><a class="nav-link" href="{{ url('/') }}" style="color: #ffffff;">&nbsp &nbsp Contactenos &nbsp &nbsp<i class="material-icons right">local_phone</i></a></li>
+                        {{--  Login  --}}
+                        <li class="nav-item"><a class="nav-link" href="{{ url('/login') }}" style="color: #ffffff;">&nbsp &nbsp Login &nbsp &nbsp<i class="material-icons right">account_circle</i></a></li>
 
-                </ul>
+                    </ul>
+                </div>
             </div>
         </nav>
