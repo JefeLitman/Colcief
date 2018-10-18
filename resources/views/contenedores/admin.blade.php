2e193dd030612bdd778a157df92aaf3aa8ad855d
eduardcaballero <eduardcaballerop@gmail.com>
2018-10-18 15:59:29 -0500

    act
    act

--- resources/views/contenedores/admin.blade.php
+++ resources/views/contenedores/admin.blade.php
@@ -7,28 +7,13 @@
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}"  media="screen,projection"/>
-      <!--precarga-->
-      <link type="text/css" rel="stylesheet" href="{{ asset('css/preload.css') }}"/>
       <!-- Definiendo el titulo de la pagina -->
       <title>ColCIEF - @yield('titulo')</title>
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
-        <!-- Precarga-->
-        <div class="preloader-background" id="preloader-background">
-            <div class="preloader-wrapper big active">
-                <div class="spinner-layer spinner-blue">
-                  <div class="circle-clipper left">
-                    <div class="circle"></div>
-                  </div><div class="gap-patch">
-                    <div class="circle"></div>
-                  </div><div class="circle-clipper right">
-                    <div class="circle"></div>
-                  </div>
-                </div>
-            </div>
-        </div>
+
     <!-- En esta parte va el menu con la directiva includee para que quede en el lugar -->
     @guest
         @include('menus.admin')
--- resources/views/contenedores/principal.blade.php
+++ resources/views/contenedores/principal.blade.php
@@ -8,8 +8,6 @@
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="{{ asset('css/materialize.min.css') }}"  media="screen,projection"/>
-      <!--precarga-->
-      <link type="text/css" rel="stylesheet" href="{{ asset('css/preload.css') }}"/>
       <!-- Definiendo el titulo de la pagina -->
       <title>ColCIEF - @yield('titulo')</title>
       <!--Let browser know website is optimized for mobile-->
@@ -17,20 +15,6 @@
       <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
-    <!-- Precarga-->
-        <div class="preloader-background" id="preloader-background">
-            <div class="preloader-wrapper big active">
-                <div class="spinner-layer spinner-blue">
-                  <div class="circle-clipper left">
-                    <div class="circle"></div>
-                  </div><div class="gap-patch">
-                    <div class="circle"></div>
-                  </div><div class="circle-clipper right">
-                    <div class="circle"></div>
-                  </div>
-                </div>
-            </div>
-        </div>
     <!-- En esta parte va el menu con la directiva includee para que quede en el lugar -->
     @include('menus.principal')
     <main>
