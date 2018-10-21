c2c964eb99959de3061970cd8d908ebd8a1a2a5e
eduardcaballero <eduardcaballerop@gmail.com>
2018-10-21 16:33:33 -0500

    quitando
    se quitola descripcion de las imagenes

--- resources/views/pantallas/inicio.blade.php
+++ resources/views/pantallas/inicio.blade.php
@@ -12,39 +12,39 @@
                 <div class="carousel-inner" role="listbox">
                 <div class="carousel-item active">
                 <img class="d-block img-fluid" src="css/img/s1.jpg" alt="First slide">
-                <div class="carousel-caption d-none d-md-block">
+                {{-- <div class="carousel-caption d-none d-md-block">
                     <div class="banner-text">
                         <h2>This is Heaven</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
                     </div>	
-                </div>
+                </div> --}}
                 </div>
                 <div class="carousel-item">
                 <img class="d-block img-fluid" src="css/img/s2.jpg" alt="Second slide">
-                <div class="carousel-caption d-none d-md-block">
+                {{-- <div class="carousel-caption d-none d-md-block">
                     <div class="banner-text">
                         <h2>This is Heaven</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
                     </div>	
-                </div>
+                </div> --}}
                 </div>
                 <div class="carousel-item">
                     <img class="d-block img-fluid" src="css/img/s3.jpg" alt="First slide">
-                    <div class="carousel-caption d-none d-md-block">
+                    {{-- <div class="carousel-caption d-none d-md-block">
                         <div class="banner-text">
                             <h2>This is Heaven</h2>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
                         </div>	
-                    </div>
+                    </div> --}}
                 </div>
                 <div class="carousel-item">
                     <img class="d-block img-fluid" src="css/img/s4.jpg" alt="First slide">
-                    <div class="carousel-caption d-none d-md-block">
+                    {{-- <div class="carousel-caption d-none d-md-block">
                         <div class="banner-text">
                             <h2>This is Heaven</h2>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
                         </div>	
-                    </div>
+                    </div> --}}
                 </div>
 
 
