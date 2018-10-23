7f2710b39309b144c26b2e503b95abb9cb435294
juanse1080 <juanmarcon1080@gmail.com>
2018-10-23 16:35:39 -0500

    terminal

--- app/Http/Controllers/Terminal.php
+++ app/Http/Controllers/Terminal.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class Terminal extends Controller{
     public function link(){
-        $salida = shell_exec('cd ColCief && php "/opt/lampp/htdocs/ColCief/artisan" serve --host=localhost --port=80');
+        $salida = shell_exec('cd ColCief && php artisan serve --host=localhost --port=80');
         return "<pre>".$salida."</pre>";
     }
 }
