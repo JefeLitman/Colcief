8e781da32394d21fa1cca82c794a777ffa7a6613
juanse1080 <juanmarcon1080@gmail.com>
2018-10-23 16:27:33 -0500

    Terminal

--- app/Http/Controllers/Terminal.php
+++ app/Http/Controllers/Terminal.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class Terminal extends Controller{
     public function link(){
-        $salida = shell_exec('cd .. && dir');
+        $salida = shell_exec('cd .. && php artisan storage:link');
         return "<pre>".$salida."</pre>";
     }
 }
