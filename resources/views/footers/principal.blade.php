bcfabf914a77243b1ad060b48b06e9a04062e433
eduardcaballero <eduardcaballerop@gmail.com>
2018-11-01 21:32:25 -0500

    footer
    se adapta al contenido

--- resources/views/footers/principal.blade.php
+++ resources/views/footers/principal.blade.php
@@ -1,5 +1,5 @@
 <!-- Footer de la pagina principal -->
-
+<div class='main'></div>
 <footer>
     <div class="text-center lead" style="width:100%;color: #fff; background-color: #556270;padding:5px;left: 0;bottom: 0;">
         <p  style="font-size: 1.5rem;">Colegio Integrado Ezequiel Florián</p>
