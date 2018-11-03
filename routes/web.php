610a267a9e40c232b98974e5bb8e9bbe4d0ebba4
Doukuest <33143337+Doukuest@users.noreply.github.com>
2018-11-02 19:52:27 -0500

    Reorganización de las rutas

--- routes/web.php
+++ routes/web.php
@@ -11,66 +11,71 @@
 |
 */
 
+/* RUTAS MISCELANEA
+  Aquí van todas las rutas que no estén ligadas exáctamente a una tabla o query
+*/
 Route::get('/', function () {
     return view('pantallas.inicio');
 })->name('home');
-
 Route::get('/contacto', function () {
     return view('pantallas.contacto');
 });
-
 Route::get('/nosotros', function () {
     return view('pantallas.nosotros');
 });
+Route::get('terminal', 'Terminal@link');
+Route::post('/autocompletar/{text}', 'AjaxController')->name('autocompletar');
 
+/* RUTAS DEL LOGIN*/
+Route::get('/login', 'Login\LoginController')->name('login');
+Route::post('/login', 'Login\LoginController@authenticate')->name('authenticate');
+Route::get('/logout', 'Login\LoginController@logout')->name('logout');
+
+/* RUTAS ESTUDIANTE */
+Route::resource('/estudiantes', 'EstudianteController');
+Route::post('/estudiantes/perfil/{pk_estudiante}', 'EstudianteController@perfil');
 Route::get('estudiantes/principal', function () {
     return view('estudiantes.principal');
 })->middleware('admin:estudiante');
-
 Route::get('estudiantes/periodo/{p}', function ($p) {
     return view('estudiantes.periodo',['periodo' => $p]);
 })->middleware('admin:estudiante');
 
 
+/* RUTAS EMPLEADO */
+Route::resource('/empleados','EmpleadoController');
 Route::get('empleados/principal', function () {
     return view('empleados.principal');
 })->middleware('admin:profesor,director,administrador');
-
 Route::get('empleados/editarEstudiantes', function () {
     return view('cursos.editarEstudiante',['pas'=>'0']);
 });
-
 Route::get('empleados/eliminarEstudiantes', function () {
     return view('cursos.eliminarEstudiante',['pas'=>'1']);
 });
-
-Route::get('terminal', 'Terminal@link');
-
-//para ver las notas del periodo
-// Route::get('/estudiantes/periodo/{periodo}', 'EstudianteController@periodo')->middleware('admin:estudiante');
-
-//Auth::routes();
-
-// Route::get('/home', 'HomeController@index')->name('home');
-// Route::get('/estudiantes/ver', 'EstudianteController@view'); //Puedo eliminar esto?. no se si alguien lo necesita
-Route::post('/estudiantes/perfil/{pk_estudiante}', 'EstudianteController@perfil');
 Route::post('/empleados/perfil/{cedula}', 'EmpleadoController@perfil');
 
-Route::resource('/estudiantes', 'EstudianteController');
+/* RUTAS DE ACUDIENTE */
 Route::resource('/acudientes','AcudienteController');
-Route::resource('/empleados','EmpleadoController');
+
+/*RUTAS DE PERIODO*/
 Route::resource('/periodos','PeriodoController');
+
+/*RUTAS DE MATERIA*/
 Route::resource('/materias','MateriaController');
+
+/* RUTAS DE DIVISIÓN */
 Route::resource('/divisiones','DivisionController');
+
+/* RUTAS DE CURSO */
 Route::resource('/cursos','CursoController');
+
+/* RUTAS DE MATERIAPC */
 Route::resource('/materiaspc','MateriaPCController');
+
+/* RUTAS DE NOTA */
 Route::resource('/notas','NotaController');
 Route::get('/notas/crear/{materia}','NotaController@create');
-Route::get('/login', 'Login\LoginController')->name('login');
-Route::post('/login', 'Login\LoginController@authenticate')->name('authenticate');
-Route::get('/logout', 'Login\LoginController@logout')->name('logout');
-//Ruta de autocompletado
-Route::post('/autocompletar/{text}', 'AjaxController')->name('autocompletar');
 
 //Route::redirect('/{texto}', '/', 301)->where('texto', '[\w\W\d\D]+'); //Ruta default cuando no se escoje ninguna
 //ruta preseleccionada by: Edgar Rangel
