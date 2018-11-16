3387cd893e98b6f9fb9206d0aac0c837a65990f8
piiok <33143055+piiok@users.noreply.github.com>
2018-11-15 20:21:38 -0500

    Request Empleado Reactualizado

--- app/Http/Requests/EmpleadoUpdateController.php
+++ app/Http/Requests/EmpleadoUpdateController.php
@@ -24,14 +24,13 @@ class EmpleadoUpdateController extends FormRequest
     public function rules(){
         return [
             'cedula' => 'required|numeric',
-            'nombre' => 'required|string|max:20',
-            'apellido' => 'required|string|max:20',
-            'correo' => 'required|email|max:40',
-            'direccion' => 'required|string|max:20',
+            'nombre' => 'required|string|max:50',
+            'apellido' => 'required|string|max:50',
+            'correo' => 'required|email|max:50',
+            'direccion' => 'required|string|max:255',
             'titulo' => 'required|string|max:50',
             'role' => 'required|string|max:1',
             'fk_curso' => 'numeric',
-            'director' => 'string|max:255',
             'estado' => 'boolean',
             'foto' => 'image|mimes:jpeg,bmp,png,jpg'
         ];
