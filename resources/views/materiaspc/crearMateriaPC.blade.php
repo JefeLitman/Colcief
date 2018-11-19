f955df18199170c0ec0e8e15a956ad0e819a76e7
Karen <karen_vivianasis@hotmail.com>
2018-11-19 14:38:22 -0500

    crear y ver materiaspc

--- resources/views/materiaspc/crearMateriaPC.blade.php
+++ resources/views/materiaspc/crearMateriaPC.blade.php
@@ -1,12 +1,8 @@
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+@extends('contenedores.admin')
+@section('titulo','Materia-Profesor-Curso Nuevo')
+@section('contenedor_admin')
+{{-- mensajes de error --}}
+@include('error.error')
 {{-- Guia Front --}}
 {{-- Se envia $cursos, $materias, $profesores --}}
 {{-- Los empleados qie se envian --}}
@@ -16,37 +12,96 @@
 
 {{-- Acceso: Solo del Administrador. --}}
 
-<b>Contenido $cursos: </b>{{$cursos}} <br><br>
+{{-- <b>Contenido $cursos: </b>{{$cursos}} <br><br>
 <b>Contenido $materias: </b>{{$materias}} <br><br>
-<b>Contenido $profesores: </b>{{$profesores}} <br><br>
+<b>Contenido $profesores: </b>{{$profesores}} <br><br> --}}
+<br>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-10">
+            <form method="post" action="/materiaspc" onload="logros(document.getElementById('fk_materia').value)">
+                @csrf
+                <div class="card border-primary rounded-0" style="border-color:#66bb6a !important;">
+                    <div class="card-header p-0">
+                       <div class="bg-info text-white text-center py-2" style="background-color:#66bb6a !important;">
+                           <h3><i class="fas fa-chalkboard-teacher"></i> Crear Materia - Profesor - Curso</h3>
+                       </div>
+                    </div>
 
-<form method="post" action="/materiaspc" onload="logros(document.getElementById('fk_materia').value)">
-    @csrf
-    <h1>CREAR MATERIA PROFESOR-CURSO</h1>
-    
-    Materia: 
-    <select name="fk_materia" id="fk_materia" onchange="logros(this.value)" required>
-        @foreach ( $materias as $materia)
-        <option value="{{$materia["pk_materia"]}}">{{$materia["nombre"]}}</option>
-        @endforeach
-    </select> <br><br>
-    Profesor: 
-    <select name="fk_empleado" id="fk_empleado" required>
-        @foreach ( $profesores as $profesor)
-        <option value="{{$profesor["cedula"]}}">{{ucwords($profesor["nombre"])}} {{ucwords($profesor["apellido"])}}</option>
-        {{-- Los nombres y apellidos estan normalizados a estar completamente en minuscula --}}
-        @endforeach
-    </select> <br><br>
-    Curso: 
-    <select name="fk_curso" id="fk_curso" required>
-        @foreach ( $cursos as $curso)
-        <option value="{{$curso["pk_curso"]}}">{{$curso["nombre"]}}</option>
-        @endforeach
-    </select> <br><br> 
+                <div class="card-body p-3">
+                    <div class="row">
+                        {{-- Datos de la materia --}}
+                        <div class="col-md-6">
+                            <div class="input-group mb-2">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                        <i class="fas fa-book"></i>
+                                    </span>
+                                </div>
+                            <select class="custom-select custom-select-sm" name="fk_materia" id="fk_materia" onchange="logros(this.value)" required>
+                                <option if>Seleccionar la materia</option>
+                                @foreach ( $materias as $materia)
+                                    <option value="{{$materia["pk_materia"]}}">{{$materia["nombre"]}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    {{-- profesor --}}
+                    <div class="col-md-6">
+                        <div class="input-group mb-2">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">
+                                    <i class="fas fa-user-tie"></i>
+                                </span>
+                            </div>
+                            <select class="custom-select custom-select-sm" name="fk_empleado" id="fk_empleado" required>
+                                <option if>Seleccionar profesor</option>
+                                @foreach ( $profesores as $profesor)
+                                    <option value="{{$profesor["cedula"]}}">{{ucwords($profesor["nombre"])}} {{ucwords($profesor["apellido"])}}</option>
+                                    {{-- Los nombres y apellidos estan normalizados a estar completamente en minuscula --}}
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                </div>
 
-    {{-- El contenido no debe exceder los 5 caracteres --}}
-    Salon: <input type="text" name="salon" id="salon" maxlength="5" required>
-    <br>
-    <br>
-    <button type="submit">Guardar</button>
-</form>
+                <div class="row">
+                    {{-- curso --}}
+                    <div class="col-md-6">
+                        <div class="input-group mb-2">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">
+                                    <i class="fas fa-pen"></i>
+                                </span>
+                            </div>
+                            <select class="custom-select custom-select-sm" name="fk_curso" id="fk_curso" required>
+                                <option if>Seleccionar el curso</option>
+                                @foreach ( $cursos as $curso)
+                                    <option value="{{$curso["pk_curso"]}}">{{$curso["nombre"]}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    {{-- salon --}}
+                    <div class="col-md-6">
+                        <div class="input-group mb-2">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">
+                                    <i class="fas fa-chalkboard-teacher"></i>
+                                </span>
+                            </div>
+                            <input type="text" placeholder="Salón (máx 5 caracteres)" class="form-control form-control-sm" name="salon" id="salon" maxlength="5" required>
+                        </div>
+                    </div>
+                </div>
+                {{-- Boton de enviar --}}
+                <div class="text-center">
+                    <input type="submit" name="action" value="Enviar" class=" btn btn-info btn-block rounded-0 py-2 " style="background-color: #66bb6a !important; border-color: #66bb6a !important;">
+                </div>
+            </form>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+@endsection
