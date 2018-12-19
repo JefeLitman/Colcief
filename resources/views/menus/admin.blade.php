370849e0dedd07ee3653965084b343603ac0ba0d
caballero <eduardcaballerop@gmail.com>
2018-12-19 11:53:46 -0500

    menu

--- resources/views/menus/admin.blade.php
+++ resources/views/menus/admin.blade.php
@@ -121,23 +121,29 @@
 
         <!-- Page Content  -->
         <div id="content">
-
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
-					<div class="sidebar-header" id="sidebarCollapse" style="cursor: pointer;">
-						<h3><i class="fas fa-bars"></i></h3>
-					</div>
+
+                    <button type="button" id="sidebarCollapse" class="btn btn-info">
+                        <i class="fas fa-align-left"></i>
+                    </button>
+                    <button class="btn btn-info d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-bars"></i>
+                    </button>
 
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="nav navbar-nav ml-auto">
                             <li class="nav-item active">
-								<span id="notificaciones" class="badge badge-pill badge-secondary">0</span>
                                 <a class="nav-link" href="{{ url('empleados/principal') }}">{{ucwords(session('user')['nombre'])}} {{ucwords(session('user')['apellido'])}} <i class="fas fa-user-circle"></i></a>
+                    
+                            </li>
+                            <li class="nav-item">
+								<a class="nav-link active" href="#">Notificaciones <span id="notificaciones" class="badge badge-pill badge-secondary">0</span> </a>
                             </li>
                         </ul>
                     </div>
                 </div>
-			</nav>
+            </nav>
 
 
 
