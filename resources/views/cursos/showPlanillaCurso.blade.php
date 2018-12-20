7a58476731f2a4ec8d51717215ef72bea236e278
caballero <eduardcaballerop@gmail.com>
2018-12-20 14:30:34 -0500

    act planilla curso

--- resources/views/cursos/showPlanillaCurso.blade.php
+++ resources/views/cursos/showPlanillaCurso.blade.php
@@ -24,132 +24,104 @@
                         </tr>
                     </tbody>
                 </table>
-          <h5 class="card-title">Listado</h5>
           <p class="card-text">
             <div class="table-responsive">
-            <table class="table table-striped  table-hover ">
+            <table class="table table-striped table-condensed table-sm  table-hover text-center">
                 <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Nombre</th>
-                    <th scope="col">IA</th>
-                    <th scope="col" colspan="2">Saber Saber</th>
-                    <th scope="col" colspan="2">Saber Hacer</th>
-                    <th scope="col" colspan="2">Saber Ser</th>
-                  </tr>
+                    <tr class="table-primary" >
+                        <th rowspan="2">Nombres</th>
+                        <th rowspan="2">IA</th>
+                        @foreach ($divisiones as $d)
+                        {{-- Encabezado: Nota de  la division --}}
+                        <th data-toggle="tooltip" data-placement="bottom" title="{{$d->porcentaje}}%" colspan="{{count($notas[$p->pk_periodo][$d->pk_division])}}" class="table-primary">
+                            {{$d->nombre}} <span class="badge badge-pill badge-primary">{{$d->porcentaje}}%</span>
+                        </th>
+                        <th></th>
+                        @endforeach
+                    <th rowspan="2" data-toggle="tooltip" data-placement="bottom" title="Nota Periodo {{$p->nro_periodo}}" >NP{{$p->nro_periodo}}</th>
+                        <th rowspan="2" data-toggle="tooltip" data-placement="bottom" title="Nota definitiva del año" >NF</th>
+                    </tr>
+                    <tr>
+                        @foreach ($divisiones as $d)
+                        
+                            @foreach ($notas[$p->pk_periodo][$d->pk_division] as $n)
+                                {{-- Encabezado: Notas--}}
+                                
+                                <th class="table-secondary" data-toggle="tooltip" data-placement="bottom" title="{{$n->porcentaje}}%" >
+                                    {{$n->nombre}} <span class="badge badge-pill badge-secondary">{{$n->porcentaje}}%</span>
+                                </th>
+                            @endforeach
+                        <th class="table-secondary"></th>
+                        @endforeach
+                    </tr>
+               
                 </thead>
                 <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                  </tr>
+                        @foreach ($estudiantes as $e)
+                        <tr>
+                            <td>
+                                {{-- Nombre del estudiante --}}
+                                {{$e->nombre}} {{$e->apellido}}
+                            </td>
+                            <td>
+                                {{-- Numero de inasistencias por el periodo seleccionado del estudiante --}}
+                                {{$notaPer[$e->pk_estudiante][$p->pk_periodo]->inasistencias}}
+                            </td>
+                                @foreach ($divisiones as $d)
+                                    @foreach ($notas[$p->pk_periodo][$d->pk_division] as $n)
+                                        <td>
+                                            {{-- Notas del estudiante --}}
+                                            {{$notaE[$e->pk_estudiante][$p->pk_periodo][$d->pk_division][$n->pk_nota]->nota}}
+                                        </td>
+                                    @endforeach
+                                    <td data-toggle="tooltip" data-placement="bottom" @if ($notaDiv[$e->pk_estudiante][$p->pk_periodo][$d->pk_division]->nota_division <= 2.9)
+                                        class="table-danger"  title="Nota Baja"
+                                    @elseif($notaDiv[$e->pk_estudiante][$p->pk_periodo][$d->pk_division]->nota_division <= 3.9)
+                                        class="table-warning"  title="Nota Basica"
+                                    @elseif($notaDiv[$e->pk_estudiante][$p->pk_periodo][$d->pk_division]->nota_division <= 4.5)
+                                        class="table-primary"  title="Nota Alta"
+                                    @else
+                                        class="table-success" title="Nota Superior"
+                                    @endif >
+                                        {{-- Notas por division del estudiante --}}
+                                        {{$notaDiv[$e->pk_estudiante][$p->pk_periodo][$d->pk_division]->nota_division}}
+                                    </td>
+                                @endforeach
+                                <td data-toggle="tooltip" data-placement="bottom" @if ($notaPer[$e->pk_estudiante][$p->nro_periodo]->nota_periodo <= 2.9)
+                                    class="table-danger"  title="Nota Periodo Baja"
+                                    @elseif($notaPer[$e->pk_estudiante][$p->nro_periodo]->nota_periodo <= 3.9)
+                                        class="table-warning"  title="Nota Periodo Basica"
+                                    @elseif($notaPer[$e->pk_estudiante][$p->nro_periodo]->nota_periodo <= 4.5)
+                                        class="table-primary"  title="Nota Periodo Alta"
+                                    @else
+                                        class="table-success" title="Nota Periodo Superior"
+                                    @endif >
+                                {{-- Notas por periodo del estudiante --}}
+                                {{$notaPer[$e->pk_estudiante][$p->nro_periodo]->nota_periodo}}
+                            </td>
+                            <td data-toggle="tooltip" data-placement="bottom"  @if ($e->nota_materia <= 2.95)
+                                class="table-danger"  title="Nota Final Baja"
+                                    @elseif($e->nota_materia <= 3.9)
+                                        class="table-warning"  title="Nota Final Basica"
+                                    @elseif($e->nota_materia <= 4.5)
+                                        class="table-primary"  title="Nota Final Alta"
+                                    @else
+                                        class="table-success" title="Nota Final Superior"
+                                    @endif >
+                                {{-- Nota final del estudiante --}}
+                                {{$e->nota_materia}}
+                            </td>
+                        </tr>
+                    @endforeach
                 </tbody>
               </table>
             </div>
           </p>
         </div>
       </div>
-    <div>
-        <h5 class="text-center"> COLEGIO INTEGRADO EZEQUIEL FLORIAN </h5>
-        <h5 class="text-center">FLORIAN - SANTANDER </h5>
-        <b>Materia: </b> {{ucwords($materiapc->materia)}}
-        <b>Curso: </b> {{$g[$materiapc->prefijo]}} - {{$materiapc->sufijo}}
-        <b>Año: </b> {{$materiapc->created_at->year}}
-        <br>
-        <b>Docente: </b> {{ucwords($materiapc->nombre)}} {{ucwords($materiapc->apellido)}}
-        <b>Periodo: </b> {{strtoupper($g[$p->nro_periodo])}}
-    </div>
-    <br>
-
-    {{-- Planilla --}}
-    <table>
-        {{-- Encabezado planilla --}}
-        <thead>
-            <tr>
-                <th>
-                    Nombre estudiante
-                </th>
-                <th>
-                    {{-- Inasistencias por el periodo seleccionado --}}
-                    IA
-                </th>
-                    @foreach ($divisiones as $d)
-                        @foreach ($notas[$p->pk_periodo][$d->pk_division] as $n)
-                            {{-- Encabezado: Notas--}}
-                            <th>
-                                {{$n->nombre}} ({{$n->porcentaje}}%)
-                            </th>
-                        @endforeach
-                        {{-- Encabezado: Nota de  la division --}}
-                        <th>
-                            {{$d->nombre}} ({{$d->porcentaje}}%)
-                        </th>
-                    @endforeach
-                    @foreach ($periodos as $z)
-                        {{-- Encabezado: Notas periodos --}}
-                        <th>
-                            P{{$z->nro_periodo}} {{-- Periodo N --}}
-                        </th>
-                    @endforeach
-                <th>
-                    {{-- Encabezado: Nota Final --}}
-                    NF
-                </th>
-            </tr>
-        </thead>
-        {{-- Cuerpo planilla --}}
-        <tbody>
-            @foreach ($estudiantes as $e)
-                <tr>
-                    <td>
-                        {{-- Nombre del estudiante --}}
-                        {{$e->nombre}} {{$e->apellido}}
-                    </td>
-                    <td>
-                        {{-- Numero de inasistencias por el periodo seleccionado del estudiante --}}
-                        {{$notaPer[$e->pk_estudiante][$p->pk_periodo]->inasistencias}}
-                    </td>
-                        @foreach ($divisiones as $d)
-                            @foreach ($notas[$p->pk_periodo][$d->pk_division] as $n)
-                                <td>
-                                    {{-- Notas del estudiante --}}
-                                    {{$notaE[$e->pk_estudiante][$p->pk_periodo][$d->pk_division][$n->pk_nota]->nota}}
-                                </td>
-                            @endforeach
-                            <td>
-                                {{-- Notas por division del estudiante --}}
-                                {{$notaDiv[$e->pk_estudiante][$p->pk_periodo][$d->pk_division]->nota_division}}
-                            </td>
-                        @endforeach
-                        @foreach ($periodos as $z)
-                            <td>
-                                {{-- Notas por periodo del estudiante --}}
-                                {{$notaPer[$e->pk_estudiante][$z->pk_periodo]->nota_periodo}}
-                            </td>
-                        @endforeach
-                    <td>
-                        {{-- Nota final del estudiante --}}
-                        {{$e->nota_materia}}
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-
-    </table>
-
-</div>
+    <script>
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+        })
+    </script>
 @endsection
