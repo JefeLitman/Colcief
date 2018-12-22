cba95fa90e0b8f3497315d4c52fffab8ef12f359
piiok <33143055+piiok@users.noreply.github.com>
2018-12-22 17:30:28 -0500

    Update Nota.php

--- app/Nota.php
+++ app/Nota.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use App\NotaEstudiante;
 use App\MateriaBoletin;
+use App\NotaDivision;
 
 class Nota extends Model
 {
@@ -29,9 +30,35 @@ class Nota extends Model
     }
 
     public function crearNotasEstudiante(){
-      $notas=MateriaBoletin::select("nota_division.pk_nota_division")->join('nota_periodo','nota_periodo.fk_materia_boletin','=','materia_boletin.pk_materia_boletin')->join('nota_division'.'nota_division.fk_nota_periodo','=','nota_periodo.pk_nota_periodo')->where([['materia_boletin.fk_materia_pc',$this->fk_materia_pc],['nota_periodo.fk_periodo',$this->fk_periodo],['nota_division.fk_division',$this->fk_division]])->groupBy('pk_nota_division');
-      foreach($notas as $n){
+      $notasE=MateriaBoletin::select("nota_division.pk_nota_division")->join('nota_periodo','nota_periodo.fk_materia_boletin','=','materia_boletin.pk_materia_boletin')->join('nota_division'.'nota_division.fk_nota_periodo','=','nota_periodo.pk_nota_periodo')->where([['materia_boletin.fk_materia_pc',$this->fk_materia_pc],['nota_periodo.fk_periodo',$this->fk_periodo],['nota_division.fk_division',$this->fk_division]])->groupBy('pk_nota_division');
+      foreach($notasE as $n){
         NotaEstudiante::create(['fk_nota_division'=>$n->pk_nota_division,'fk_nota'=>$this->pk_nota]);
+      }
+    }
+
+    public function cambioPorcentaje(){
+      $notasE=NotaEstudiante::select('fk_nota_division')->where('fk_nota',$this->pk_nota)->get();
+      foreach($notasE as $n){
+        $n->ViejaizarNota();
+      }
+    }
+
+    public function cambioDivision(){
+      $notasE=NotaEstudiante::where('fk_nota',$this->pk_nota)->get();
+      foreach ($notasE as $n) {
+        $notaDivVieja=NotaDivision::where('pk_nota_division',$n->fk_nota_division)->get();
+        if (!empty($notaDivVieja[0])) {
+          $notaDivVieja=$notaDivVieja[0];
+          $notaDivNueva=NotaDivision::where([['fk_nota_periodo',$notaDivVieja->fk_nota_periodo],['fk_division',$this->fk_division]])->get();
+          if (!empty($notaDivNueva[0])) {
+            $notaDivNueva=$notaDivNueva[0];
+            $n->fk_nota_division=$notaDivNueva->pk_nota_division;
+            $n->save();
+            $notaDivVieja->actualizarNota();
+            $notaDivNueva->actualizarNota();
+          }
+        }
+        
       }
     }
 }
