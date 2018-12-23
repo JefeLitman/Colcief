7464ce23a5d351774940567d4401ba0ee992ec7e
Karen <karen_vivianasis@hotmail.com>
2018-12-22 22:42:06 -0500

    Arreglando notas
    Todavia falta arreglar bien la tabla de notas

--- resources/views/notas/indexNota.blade.php
+++ resources/views/notas/indexNota.blade.php
@@ -1,36 +1,46 @@
-<!DOCTYPE html>
-<html lang="es" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <table border="2">
-      <tr>
-        <th>Componente</th>
-        <th>Notas pertenecientes al corte</th>
-      </tr>
-      @foreach ($divisiones as $nombre => $division)
-        <tr>
-          <td>{{$nombre}}</td>
-          <td>
-            <table>
-              <tr>
-                <th>Pk_nota</th>
-                <th>Nombre nota</th>
-                <th>Porcentaje</th>
-              </tr>
-              @foreach ($division as $nota)
-                <tr>
-                  <td>{{$nota['pk_nota']}}</td>
-                  <td>{{$nota['nombre']}}</td>
-                  <td>{{$nota['porcentaje']}}</td>
-                </tr>
-              @endforeach
+@extends('contenedores.'.((session('role')=='administrador')?'admin':'profesor'))
+@section('contenedor_'.((session('role')=='administrador')?'admin':'profesor'))
+@section('titulo','Notas')
+<br id="br">
+<div class="container" style="background:#fafafa !important;">
+    <div class="row justify-content-center" style="background-color: #fafafa !important;">
+        <h3 class="text-center mb-0">
+            Notas
+        </h3>
+        <br>
+            <div class="table-responsive">
+                <table class="table table-hover mr-auto">
+                    <thead>
+                        <tr>
+                            <th class="center" scope="col" style="color:#00695c">Componente</th>
+                            <th class="center" scope="col" style="color:#00695c">Notas pertenecientes al corte</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($divisiones as $nombre => $division)
+                        <tr>
+                            <td class="center">{{$nombre}}</td>
+                        <td>
+                    </tbody>
+                            <table>
+                            <tr>
+                                <th>Pk_nota</th>
+                                <th>Nombre nota</th>
+                                <th>Porcentaje</th>
+                            </tr>
+                            @foreach ($division as $nota)
+                                <tr>
+                                <td>{{$nota['pk_nota']}}</td>
+                                <td>{{$nota['nombre']}}</td>
+                                <td>{{$nota['porcentaje']}}</td>
+                                </tr>
+                            @endforeach
+                            </table>
+                        </td>
+                        </tr>
+                    @endforeach
             </table>
-          </td>
-        </tr>
-      @endforeach
-    </table>
-  </body>
-</html>
+        </div>
+    </div>
+</div>
+@endsection
