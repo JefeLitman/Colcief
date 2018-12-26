b39d60099a5438bdc580144057beef5a16ae5866
piiok <33143055+piiok@users.noreply.github.com>
2018-12-26 15:43:29 -0500

    Actualizacion de la estructura de notas con respecto a la tabla division

--- app/Division.php
+++ app/Division.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\NotaPeriodo;
+use App\NotaDivision;
 
 class Division extends Model{
     protected $primaryKey = "pk_division";
@@ -13,5 +15,20 @@ class Division extends Model{
     public function notas()
     {
       return $this->hasMany('App\Nota','fk_division','pk_division');
+    }
+    
+    public function crearNotasDivision(){
+      $notasPeriodo=NotaPeriodo::join('periodo','nota_periodo.fk_periodo','=','periodo.pk_periodo')->where('periodo.ano',date('Y'))->get();
+      foreach ($notasPeriodo as $i) {
+        NotaDivision::create(['fk_division'=>$this->pk_division,'fk_nota_periodo'=>$i->pk_nota_periodo]);
+      }
+    }
+
+    public function cambioPorcentaje(){
+      //Se actualizan todos los periodos
+      $periodos=NotaPeriodo::select('nota_periodo.*')->join('periodo','nota_periodo.fk_periodo','=','periodo.pk_periodo')->where('periodo.ano',date('Y'))->get();
+      foreach ($periodos as  $p) {
+        $p->actualizarNota();
+      }
     }
 }
