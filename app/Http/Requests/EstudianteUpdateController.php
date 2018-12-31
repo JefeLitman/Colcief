40a31da9244d62819bbfc65f30b45231ba53fbfc
piiok <33143055+piiok@users.noreply.github.com>
2018-12-31 15:20:57 -0500

    otro bug resuelto

--- app/Http/Requests/EstudianteUpdateController.php
+++ app/Http/Requests/EstudianteUpdateController.php
@@ -17,13 +17,14 @@ class EstudianteUpdateController extends FormRequest{
             'fecha_nacimiento' => 'required|date',
             'discapacidad' => 'boolean',
             'estado' => 'boolean',
+            'fk_curso' => 'nullable|numeric',
             'foto' => 'image|mimes:jpeg,bmp,png,jpg',
             'nombre_acu_1' => 'required|string|max:50',
             'direccion_acu_1' => 'required|string|max:255',
             'telefono_acu_1' => 'required|numeric|digits_between:7,10',
-            'nombre_acu_2' => 'string|max:50',
-            'direccion_acu_2' => 'string|max:255',
-            'telefono_acu_2' => 'numeric|digits_between:7,10'
+            'nombre_acu_2' => 'nullable|string|max:50',
+            'direccion_acu_2' => 'nullable|string|max:255',
+            'telefono_acu_2' => 'nullable|numeric|digits_between:7,10'
         ];
     }
 
