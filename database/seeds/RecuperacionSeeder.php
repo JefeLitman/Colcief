66f6dfc214f31697d48759d25586260feff508dd
piiok <33143055+piiok@users.noreply.github.com>
2019-01-23 15:51:15 -0500

    act

--- database/seeds/RecuperacionSeeder.php
+++ database/seeds/RecuperacionSeeder.php
@@ -7,7 +7,6 @@ class RecuperacionSeeder extends Seeder
 {
     public function run()
     {   
-        error_log("Entro");
         $periodos = Periodo::where('ano',date('Y'))->get();
         foreach ($periodos as $p) {
             $p->crearRecuperaciones();
