842300e4770e4d425e2375960c6ee7cde9250caa
piiok <33143055+piiok@users.noreply.github.com>
2019-01-28 18:44:19 -0500

    lista nivelaciones profesor con buscador ^^!

--- resources/views/nivelaciones/listaNivelaciones_administrador.blade.php
+++ resources/views/nivelaciones/listaNivelaciones_administrador.blade.php
@@ -1,13 +1,13 @@
-@extends('contenedores.estudiante')
+@extends('contenedores.'.((session('role')=='administrador')?'admin':(session('role'))))
+@section('contenedor_'.((session('role')=='administrador')?'admin':(session('role'))))
 @section('titulo','Nivelaciones')
-@section('contenedor_estudiante')
 
 	{{-- Guia Front --}}
 	{{-- Se envía el objeto $periodos,$recuperacion,$nivelacion--}}
 	{{-- Variables enviadas desde Local>App>Http>Controllers>NivelacionController.php  funcion index()
 		 @Autor Paola C. --}}
 	{{-- Estado: Aparentemente finalizado (Sujeto a cambios) --}}
-    {{-- URL: localhost:8000\nivelaciones  -> Logeado en un usuario de tipo estudiante--}}
+    {{-- URL: localhost:8000\nivelaciones  -> Logeado en un usuario de tipo profesor--}}
 
 	<br id="br">
 	<div class="container" style="background:#fafafa !important;">
@@ -15,6 +15,9 @@
 			<div class="col-md-11">
 				<div class="card bg-light border-info">
 					<h4 class="card-header text-center"><i class="fas fa-book"></i> Nivelaciones </h4>
+					@php
+						$bandera=true;
+					@endphp
 					<div class="card-body">
 						<ul class="nav nav-tabs" id="myTab" role="tablist">
 							@php
@@ -22,14 +25,14 @@
 							@endphp
 							@foreach ($periodos as $p)
 								<li class="nav-item">
-									<a class="nav-link @if($j==0) active @endif" id="tab{{$j}}" data-toggle="tab" href="#id{{$j}}" role="tab" aria-controls="id{{$j}}" aria-selected="true">Periodos {{$p->nro_periodo}}</a>
+									<a class="nav-link @if(strtotime(date('d-m-Y'))>=strtotime($p->recuperacion_inicio) and strtotime(date('d-m-Y'))<=strtotime($p->recuperacion_limite)) active" @php $bandera=false; @endphp @endif  id="tab{{$j}}" data-toggle="tab" href="#id{{$j}}" role="tab" aria-controls="id{{$j}}" aria-selected="true">Periodos {{$p->nro_periodo}}</a>
 								</li>
 							@php
 								$j++;
 							@endphp
 							@endforeach
 							<li class="nav-item">
-								<a class="nav-link @if($j==0) active @endif" id="tab{{$j}}" data-toggle="tab" href="#id{{$j}}" role="tab" aria-controls="id{{$j}}" aria-selected="true">Materias perdidas en definitiva</a>
+								<a class="nav-link @if($bandera) active @endif" id="tab{{$j}}" data-toggle="tab" href="#id{{$j}}" role="tab" aria-controls="id{{$j}}" aria-selected="true">Materias perdidas en definitiva</a>
 							</li>
 						</ul>
 						<div class="tab-content" id="myTabContent">
@@ -37,13 +40,18 @@
 								$k=0;
 							@endphp
 							@foreach ($periodos as $p)
-							<div class="tab-pane fade @if($k==0) show active @endif" id="id{{$k}}" role="tabpanel" aria-labelledby="tab{{$k}}">
+							<div class="tab-pane fade 
+							@if(strtotime(date('d-m-Y'))>=strtotime($p->recuperacion_inicio) and strtotime(date('d-m-Y'))<=strtotime($p->recuperacion_limite)) 
+							show active 
+							@endif" 
+							id="id{{$k}}" role="tabpanel" aria-labelledby="tab{{$k}}">
 								<div class="table-responsive">
 										<table class="table table-striped table-condensed table-sm  table-hover text-center">
 											<thead>
 												<tr>
 													<th scope="col" style="color:#00695c" class="text-center"> Materia </th>
-													<th scope="col" style="color:#00695c" class="text-center"> Profesor </th>
+													<th scope="col" style="color:#00695c" class="text-center"> Estudiante </th>
+													<th scope="col" style="color:#00695c" class="text-center"> Curso/Año </th>
 													<th scope="col" style="color:#00695c" class="text-center"> Nota </th>
 													<th scope="col" style="color:#00695c" class="text-center"> Acciones </th>
 												</tr>
@@ -51,19 +59,28 @@
 											<tbody>	
 												@if (count($recuperacion[$p->pk_periodo])==0)
 													<tr>
-														<td colspan="4" class="text-center"> No hay periodos perdidos </td>
+														<td colspan="5" class="text-center"> No hay periodos perdidos </td>
 													</tr>
 												@else
 													@foreach ($recuperacion[$p->pk_periodo] as $r)
 														<tr>	
 															{{-- Materia --}}
 															<td class="text-center"> {{$r->materia}} </td>
-															{{-- Profesor --}}
-															<td class="text-center"> {{ucwords($r->nombre)}} {{ucwords($r->apellido)}}</td>
+															{{-- Estudiante --}}
+															<td class="text-center"> {{ucwords($r->apellido)}} {{ucwords($r->nombre)}}</td>
+															{{-- Curso --}}
+															<td class="text-center"> {{($r->prefijo==0)?"Preescolar":$r->prefijo}}-{{$r->sufijo}} / {{$r->ano}}</td>
 															{{-- Nota --}}
 															<td class="text-center"> {{$r->nota or "-"}} </td>
 															{{-- Accion --}}
-															<td class="text-center"><a href="/recuperaciones/{{$r->pk_recuperacion}}" data-toggle="tooltip" data-placement="right" title="Ver más"><i class="fas fa-eye text-info"  ></i></a></td>
+															<td class="text-center">
+																<a href="/recuperaciones/{{$r->pk_recuperacion}}"><i class="fas fa-eye text-info"  title="Ver más"></i></a>
+																@if (strtotime(date('d-m-Y'))>=strtotime($p->recuperacion_inicio) and strtotime(date('d-m-Y'))<=strtotime($p->recuperacion_limite))
+																	<a href="/recuperaciones/{{$r->pk_recuperacion}}/editar"><i class="fas fa-edit text-info"></i></a>
+																@else
+																	<a href="" title="Solo puede ser editado desde el {{$p->recuperacion_inicio}} hasta las 23:59 del {{$p->recuperacion_limite}}."><i class="fas fa-edit  text-secondary"></i></a>
+																@endif
+															</td>
 														</tr>
 													@endforeach
 												@endif
@@ -75,13 +92,13 @@
 								$k++;
 							@endphp		
 							@endforeach
-							<div class="tab-pane fade @if($k==0) show active @endif" id="id{{$k}}" role="tabpanel" aria-labelledby="tab{{$k}}">
+							<div class="tab-pane fade @if($bandera) show active @endif" id="id{{$k}}" role="tabpanel" aria-labelledby="tab{{$k}}">
 								<div class="table-responsive">
 										<table class="table table-striped table-condensed table-sm  table-hover text-center">
 											<thead>
 												<tr>
 													<th scope="col" style="color:#00695c" class="text-center"> Materia </th>
-													<th scope="col" style="color:#00695c" class="text-center"> Profesor </th>
+													<th scope="col" style="color:#00695c" class="text-center"> Estudiante </th>
 													<th scope="col" style="color:#00695c" class="text-center"> Curso/Año </th>
 													<th scope="col" style="color:#00695c" class="text-center"> Nota </th>
 													<th scope="col" style="color:#00695c" class="text-center"> Acciones </th>
@@ -97,14 +114,17 @@
 														<tr>	
 															{{-- Materia --}}
 															<td class="text-center"> {{$n->materia}} </td>
-															{{-- Profesor --}}
-															<td class="text-center"> {{ucwords($n->nombre)}} {{ucwords($n->apellido)}}</td>
+															{{-- Estudiante --}}
+															<td class="text-center"> {{ucwords($n->apellido)}} {{ucwords($n->nombre)}}</td>
 															{{-- Curso --}}
 															<td class="text-center"> {{($n->prefijo==0)?"Preescolar":$n->prefijo}}-{{$n->sufijo}} / {{$n->ano}}</td>
 															{{-- Nota --}}
 															<td class="text-center"> {{$n->nota or "-"}}</td>
 															{{-- Acciones --}}
-															<td class="text-center"><a href="/nivelaciones/{{$n->pk_nivelacion}}" data-toggle="tooltip" data-placement="right" title="Ver más" ><i class="fas fa-eye text-info"  ></i></a></td>
+															<td class="text-center">
+																<a href="/nivelaciones/{{$n->pk_nivelacion}}"><i class="fas fa-eye text-info"  title="Ver más"></i></a>
+																<a href="/nivelaciones/{{$n->pk_nivelacion}}/editar" ><i class="fas fa-edit text-info" title="Editar"></i></a>
+															</td>
 														</tr>
 													@endforeach
 												@endif
@@ -117,7 +137,4 @@
 			</div>
 		</div>
 	</div>
-	<script>
-		$('[data-toggle="tooltip"]').tooltip(); 
-	</script>
 @endsection
