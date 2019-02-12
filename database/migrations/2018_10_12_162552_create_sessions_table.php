fed0e5e7d790698f861a745eb2bd895ae748e2f0
juanse1080 <juanmarcon1080@gmail.com>
2019-02-12 09:35:30 -0500

    pruebas

--- database/migrations/2018_10_12_162552_create_sessions_table.php
+++ database/migrations/2018_10_12_162552_create_sessions_table.php
@@ -11,16 +11,16 @@ class CreateSessionsTable extends Migration {
     public function up(){
 
         Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id',180)->unique();
-            $table->unsignedInteger('user_id')->nullable();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->text('payload');
-            $table->integer('last_activity');
+            // $table->string('id',180)->unique();
+            // $table->unsignedInteger('user_id')->nullable();
+            // $table->string('ip_address', 45)->nullable();
+            // $table->text('user_agent')->nullable();
+            // $table->text('payload');
+            // $table->integer('last_activity');
         });
     }
 
     public function down(){
-        Schema::dropIfExists('sessions');
+        // Schema::dropIfExists('sessions');
     }
 }
