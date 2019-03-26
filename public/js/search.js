aaff915b5967a391766ae542b312d5179310b971
caballero <eduardcaballerop@gmail.com>
2019-03-25 23:10:51 -0500

    filtro v2
    beta

--- public/js/search.js
+++ public/js/search.js
@@ -1,3 +1,4 @@
+ant=1;
 $(document).ready(function(){
      $('#entradafilter').keyup(function () {
         var rex = new RegExp($(this).val(), 'i');
@@ -9,7 +10,10 @@ $(document).ready(function(){
         })
         var i;
 
-        $('body').on('click', '.page-link', function(){
+        $('body').on('click', '.page-item', function(){
+            $(this).addClass( "active" );
+            $("#"+ant).removeClass( "active" );
+            ant=$(this).attr('id');
             var nid=parseInt($(this).attr('id'))*2;
             $( "tr" ).hide();
             $( "tr" ).eq(0).show();
@@ -27,11 +31,16 @@ $(document).ready(function(){
         var nFilas = $("#myTable tr").length;
         $('#registros').html(nFilas+" registros");
         var bot= Math.ceil(nFilas/2);
-        var npages="<li class='page-item disabled'><a class='page-link' href='#' tabindex='-1'>Ant</a></li>";
+        // var npages="<li class='page-item disabled'><a class='page-link' href='#' tabindex='-1'>Ant</a></li>";
+        var npages="";
         for (i = 0; i < bot; i++) { 
-            npages += "<li class='page-item'><a class='page-link' id='"+(i+1)+"' data-page='"+(i+1)+"' href='#'>"+(i+1)+"</a></li>";
+            if (i==0){
+                npages += "<li class='page-item active' id='"+(i+1)+"'><a class='page-link'  href='#'>"+(i+1)+"</a></li>";
+            }else{
+                npages += "<li class='page-item' id='"+(i+1)+"'><a class='page-link' href='#'>"+(i+1)+"</a></li>";
+            }
         }
-        npages+= "<li class='page-item'><a class='page-link' href='#'>Sig</a></li>";
+        // npages+= "<li class='page-item disabled'><a class='page-link' href='#'>Sig</a></li>";
         $('#paginator').html(npages);
         
 });
