69968c03b89ce762724332aef4731e21312731bd
caballero <eduardcaballerop@gmail.com>
2019-07-01 13:10:14 -0500

    Boletin Menu
    correccion menu rol coordinador

--- resources/views/boletines/showEstudianteBoletin.blade.php
+++ resources/views/boletines/showEstudianteBoletin.blade.php
@@ -1,7 +1,7 @@
 
 
-@extends('contenedores.admin')
-@section('contenedor_admin')
+@extends('contenedores.'.((session('role')=='administrador')?'admin':(session('role'))))
+@section('contenedor_'.((session('role')=='administrador')?'admin':(session('role'))))
 @section('titulo','Estudiantes/Grado')
 {{-- Envio ["msj"=>$msj,"boletin"=>$B[0],"materias"=>$materias,"infoPeriodos"=>$infoPeriodos,"notaPeriodos"=>$notaPeriodos,"infoDivs"=>$infoDivs,"notasDiv"=>$notaDivs] --}}
 <br>
